e25585a04c92f461e3091ff56e97d6f5a57e6b4a
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-11-03 21:43:38 -0300

    extract board puts into a separate method and other basic refactors

--- game.rb
+++ game.rb
@@ -1,30 +1,44 @@
+# frozen_string_literal: true
+
+# Class Game is the main class of the game
 class Game
   def initialize
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    @com = "X" # the computer's marker
-    @hum = "O" # the user's marker
+    @board = %w[0 1 2 3 4 5 6 7 8]
+    @com = 'X' # the computer's marker
+    @hum = 'O' # the user's marker
   end
 
   def start_game
     # start by printing the board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-    puts "Enter [0-8]:"
+    show_board
+
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
-      get_human_spot
-      if !game_is_over(@board) && !tie(@board)
-        eval_board
-      end
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    until game_over(@board) || tie(@board)
+      human_spot
+
+      eval_board if !game_over(@board) && !tie(@board)
+
+      show_board
     end
-    puts "Game over"
+    puts 'Game over'
   end
 
-  def get_human_spot
+  def show_board
+    puts "
+ #{@board[0]} | #{@board[1]} | #{@board[2]}
+===+===+===
+ #{@board[3]} | #{@board[4]} | #{@board[5]}
+===+===+===
+ #{@board[6]} | #{@board[7]} | #{@board[8]}
+    "
+    puts 'Enter [0-8]:'
+  end
+
+  def human_spot
     spot = nil
     until spot
       spot = gets.chomp.to_i
-      if @board[spot] != "X" && @board[spot] != "O"
+      if @board[spot] != 'X' && @board[spot] != 'O'
         @board[spot] = @hum
       else
         spot = nil
@@ -35,12 +49,12 @@ class Game
   def eval_board
     spot = nil
     until spot
-      if @board[4] == "4"
+      if @board[4] == '4'
         spot = 4
         @board[spot] = @com
       else
-        spot = get_best_move(@board, @com)
-        if @board[spot] != "X" && @board[spot] != "O"
+        spot = best_move(@board, @com)
+        if @board[spot] != 'X' && @board[spot] != 'O'
           @board[spot] = @com
         else
           spot = nil
@@ -49,7 +63,7 @@ class Game
     end
   end
 
-  def get_best_move(board, next_player, depth = 0, best_score = {})
+  def best_move(board, next_player, depth = 0, best_score = {})
     available_spaces = []
     best_move = nil
     board.each do |s|
@@ -59,13 +73,13 @@ class Game
     end
     available_spaces.each do |as|
       board[as.to_i] = @com
-      if game_is_over(board)
+      if game_over(board)
         best_move = as.to_i
         board[as.to_i] = as
         return best_move
       else
         board[as.to_i] = @hum
-        if game_is_over(board)
+        if game_over(board)
           best_move = as.to_i
           board[as.to_i] = as
           return best_move
@@ -74,16 +88,14 @@ class Game
         end
       end
     end
-    if best_move
-      return best_move
-    else
-      n = rand(0..available_spaces.count)
-      return available_spaces[n].to_i
-    end
+
+    return best_move if best_move
+
+    n = rand(0..available_spaces.count)
+    available_spaces[n].to_i
   end
 
-  def game_is_over(b)
-
+  def game_over(b)
     [b[0], b[1], b[2]].uniq.length == 1 ||
     [b[3], b[4], b[5]].uniq.length == 1 ||
     [b[6], b[7], b[8]].uniq.length == 1 ||
@@ -95,9 +107,8 @@ class Game
   end
 
   def tie(b)
-    b.all? { |s| s == "X" || s == "O" }
+    b.all? { |s| s == 'X' || s == 'O' }
   end
-
 end
 
 game = Game.new
